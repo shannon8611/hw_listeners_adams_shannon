3436566cd22c66277d087a326c386b9e4d6872fd
shannon8611 <shannon8611@yahoo.com>
2017-11-03 21:10:03 -0500

    create js for event listener

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
-var i = 1;
-function clicker() {
-    var para = document.createElement('P');
-    var t = document.createTextNode('This is click number ' + i++);
-    para.appendChild(t);
-    document.body.appendChild(para);
-}
+var form = document.getElementById('form');
+var firstName = document.getElementById('firstname');
+var lastName = document.getElementById('lastname');
+var email = document.getElementById('email');
+var message = document.getElementById('message');
 
-document.getElementById('numclick').addEventListener('click', clicker);
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    console.log('The firstName is: ' + firstName.value);
+    console.log('The lastName is: ' + lastName.value);
+    console.log('The email is: ' + email.value);
+    console.log('The message is: ' + message.value);
+});
